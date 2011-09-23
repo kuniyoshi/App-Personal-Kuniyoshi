edb9a3a5e6bfe14caaf08fa602f9fae2d9552657
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-09-23 13:43:13 +0900

    initial commit

---
+++ lib/App/Personal/Kuniyoshi.pm
@@ -0,0 +1,32 @@
+package App::Personal::Kuniyoshi;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Personal::Kuniyoshi -
+
+=head1 SYNOPSIS
+
+  use App::Personal::Kuniyoshi;
+
+=head1 DESCRIPTION
+
+App::Personal::Kuniyoshi is
+
+=head1 AUTHOR
+
+kuniyoshi E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
