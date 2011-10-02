18847ab94a705dea348ca938859583cfbb30d616
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-10-02 17:31:18 +0900

    Can install script.

--- lib/App/Personal/Kuniyoshi.pm
+++ lib/App/Personal/Kuniyoshi.pm
@@ -1,22 +1,29 @@
 package App::Personal::Kuniyoshi;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 1;
 __END__
 
 =head1 NAME
 
-App::Personal::Kuniyoshi -
+App::Personal::Kuniyoshi - A root repository of my submodules
 
 =head1 SYNOPSIS
 
-  use App::Personal::Kuniyoshi;
+  % git clone App::Personal::Kuniyoshi
+  % git submodule init
+  % git submodule update
+  % for x in App-Personal-Kuniyoshi-Foo; do "cd $x && git checkout master"; done
+  % ###                             ^^^ Replace this to submodule name.
 
 =head1 DESCRIPTION
 
-App::Personal::Kuniyoshi is
+App::Personal::Kuniyoshi bundles my submodules.
+
+i have some scripts, and want to use those scripts in any environment.
+Before this, i copy those to ~/bin, but it is dull.
 
 =head1 AUTHOR
 
